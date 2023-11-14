c2616b33edd071829b3a052e360f0c649073bd37
DenWorker <whiteskyD@yandex.ru>
2023-11-15 00:02:53 +0300

    Solved tasks from 1.1.3 (Hibernate) and little fix from 1.1.2 (JDBC)

--- PP_1_1_3-4_JDBC_Hibernate-master/src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ PP_1_1_3-4_JDBC_Hibernate-master/src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -14,7 +14,7 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void createUsersTable() {
-        try (Connection connection = Util.createConnection();
+        try (Connection connection = Util.createConnectionJDBC_API();
              Statement statement = connection.createStatement()) {
             statement.execute("""
                     CREATE TABLE IF NOT EXISTS users (
@@ -30,7 +30,7 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void dropUsersTable() {
-        try (Connection connection = Util.createConnection();
+        try (Connection connection = Util.createConnectionJDBC_API();
              Statement statement = connection.createStatement()) {
             statement.execute("DROP TABLE IF EXISTS users");
         } catch (SQLException | ClassNotFoundException e) {
@@ -39,7 +39,7 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        try (Connection connection = Util.createConnection();
+        try (Connection connection = Util.createConnectionJDBC_API();
              PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO users (name, lastname, age) VALUES (?,?,?)")) {
             preparedStatement.setString(1, name);
             preparedStatement.setString(2, lastName);
@@ -54,7 +54,7 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void removeUserById(long id) {
-        try (Connection connection = Util.createConnection();
+        try (Connection connection = Util.createConnectionJDBC_API();
              PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM users WHERE id = ?")) {
             preparedStatement.setLong(1, id);
             preparedStatement.execute();
@@ -65,7 +65,7 @@ public class UserDaoJDBCImpl implements UserDao {
 
     public List<User> getAllUsers() {
         List<User> userList = new ArrayList<>();
-        try (Connection connection = Util.createConnection();
+        try (Connection connection = Util.createConnectionJDBC_API();
              Statement statement = connection.createStatement()) {
             ResultSet resultSet = statement.executeQuery("SELECT * FROM users");
             while (resultSet.next()) {
@@ -78,7 +78,7 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void cleanUsersTable() {
-        try (Connection connection = Util.createConnection();
+        try (Connection connection = Util.createConnectionJDBC_API();
              Statement statement = connection.createStatement()) {
             statement.execute("DELETE FROM users");
         } catch (SQLException | ClassNotFoundException e) {
