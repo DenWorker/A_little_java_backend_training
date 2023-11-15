d220a8640ce7d3a37bfe72c04d3d8114b8e9c7c1
DenWorker <whiteskyD@yandex.ru>
2023-11-16 01:08:37 +0300

    Solved tasks from 2.1.2 (beans) and loaded some tasks

---
+++ PP_2_1_2_Spring_HelloWorld-master/src/main/java/App.java
@@ -0,0 +1,29 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld beanHelloWorld1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld beanHelloWorld2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat beanCat1 = (Cat) applicationContext.getBean("cat");
+        Cat beanCat2 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println(beanHelloWorld1.getMessage());
+
+
+        System.out.println(beanHelloWorld1);
+        System.out.println(beanHelloWorld2);
+        System.out.println(beanCat1);
+        System.out.println(beanCat2);
+        System.out.println();
+
+        System.out.println(beanHelloWorld1.equals(beanHelloWorld2));
+        System.out.println(beanCat1.equals(beanCat2));
+
+    }
+}
